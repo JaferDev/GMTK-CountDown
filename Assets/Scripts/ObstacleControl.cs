0e502de741896bcf15d43204cc3e4452c9920f32
== <jaferhussain234@gmail.com>
2026-07-26 09:32:41 +0400

    Created first level and made art
    To Do:
    
    1. Make other 4 levels
    2. Make UI
    3. Debug and Playtest
    4. Music and SFX

--- Assets/Scripts/ObstacleControl.cs
+++ Assets/Scripts/ObstacleControl.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ObstacleControl : MonoBehaviour
@@ -11,17 +12,25 @@ public class ObstacleControl : MonoBehaviour
     [SerializeField] float deathTime = 2;
     [SerializeField] TimeManager timeMgr;
 
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float endTime = 10f;
+
     private bool isReturning = false;
     private float time = 0f;
+
+    private float startTime;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         winPanel.SetActive(false);
         deathPanel.SetActive(false);
+
+        startTime = Time.time;
     }
 
     private void Update()
     {
+        DisplayCountdown();
         if (!isReturning) return;
 
         time += Time.unscaledDeltaTime;
@@ -53,9 +62,15 @@ public class ObstacleControl : MonoBehaviour
         //Resetting variables
         time = 0;
         isReturning = false;
+        startTime = Time.time;
 
         //Placing player back
         rb.velocity = Vector2.zero;
         transform.position = startPos;
+    }
+
+    private void DisplayCountdown()
+    {
+        timerText.text = Mathf.FloorToInt(endTime - (Time.time - startTime)).ToString();
     }
 }
